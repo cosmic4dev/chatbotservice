177943c83745c4a00b1b87e69b86865e25731a37
cosmic4dev <57430722+cosmic4dev@users.noreply.github.com>
2023-11-07 17:22:36 +0900

    Add files via upload
    chat bot test using telegram

---
+++ telegramAPI.py
@@ -0,0 +1,16 @@
+import urllib3
+import json
+
+
+BOT_TOKEN = ""
+
+def get_updates():
+    http = urllib3.PoolManager()
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/getUpdates"
+    response = http.request('GET', url)
+    return json.loads(response.data.decode('utf-8'))
+
+# Get the latest updates
+updates = get_updates()
+
+print(updates)
